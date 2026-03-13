6443e7300b613b5156c4136acddc190bc13b28ef
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-13 21:30:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,9 @@
-// Конфигурация Lap 1x6 Pack
+// Конфигурация
 const CONFIG = {
-    // API Google Apps Script
     API_URL: 'https://script.google.com/macros/s/AKfycbwR8PJEEXPid3KUezDwPxbAQILlIWJjE2iIWaXC0eJhjQV66kb8NjpRqi9Uz6PhAVpJVg/exec',
-    
-    // GitHub raw URL (измени на свой)
     GITHUB_URL: 'https://raw.githubusercontent.com/BtheBFr/Lap-1x6-dota/main/',
-    
-    // Telegram Bot
     BOT_URL: 'https://t.me/Lap1x6Dota_bot',
-    
-    // Настройки клавиш по умолчанию
     DEFAULT_BINDS: {
-        Q: 'Q',
-        W: 'W',
-        E: 'E',
-        D: 'D',
-        F: 'F',
-        R: 'R'
-    },
-    
-    // Версия
-    VERSION: '1.0.1'
+        Q: 'Q', W: 'W', E: 'E', D: 'D', F: 'F', R: 'R'
+    }
 };
