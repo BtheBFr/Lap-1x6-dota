939f43f361ef50d3e4f49d0b39e49c6853a7ff29
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-13 21:00:46 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// Конфигурация Lap 1x6 Pack
+const CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbwR8PJEEXPid3KUezDwPxbAQILlIWJjE2iIWaXC0eJhjQV66kb8NjpRqi9Uz6PhAVpJVg/exec',
+    
+    GITHUB_URL: 'https://raw.githubusercontent.com/BtheBFr/Lap-1x6-dota/main/',
+    
+    DEFAULT_BINDS: {
+        Q: 'Q',
+        W: 'W',
+        E: 'E',
+        D: 'D',
+        F: 'F',
+        R: 'R'
+    }
+};
